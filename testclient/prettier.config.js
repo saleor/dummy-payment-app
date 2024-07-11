0774c5038e7855fcbd4c9352ddfce1940db5126f
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2024-07-11 12:52:33 +0200

    Remove test client

--- testclient/prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	semi: true,
-	singleQuote: false,
-	trailingComma: "all",
-	printWidth: 110,
-	useTabs: true,
-	plugins: [require("prettier-plugin-tailwindcss")],
-	tailwindConfig: "./tailwind.config.js",
-};
