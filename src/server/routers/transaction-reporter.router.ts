068e5b43ce051e3c4e081c503976808d647f7cc6
Jonatan Witoszek <jonatanwitoszek@gmail.com>
2024-07-12 13:40:21 +0200

    Add transaction event report impl

--- src/server/routers/transaction-reporter.router.ts
+++ src/server/routers/transaction-reporter.router.ts
@@ -1,7 +1,13 @@
 import { z } from "zod";
 import { procedureWithGraphqlClient } from "../procedure/procedure-with-graphql-client";
 import { router } from "../server";
-import { TransactionEventTypeEnum } from "../../../generated/graphql";
+import {
+  TransactionActionEnum,
+  TransactionEventReportDocument,
+  TransactionEventTypeEnum,
+} from "../../../generated/graphql";
+import { v7 as uuidv7 } from "uuid";
+import { getTransactionActions } from "@/lib/transaction-actions";
 
 export const transactionReporterRouter = router({
   reportEvent: procedureWithGraphqlClient
@@ -10,7 +16,21 @@ export const transactionReporterRouter = router({
         id: z.string(),
         amount: z.number(),
         type: z.nativeEnum(TransactionEventTypeEnum),
+        message: z.string().optional(),
       })
     )
-    .mutation(async ({ ctx, input }) => {}),
+    .mutation(async ({ ctx, input }) => {
+      const { id, amount, type, message } = input;
+
+      ctx.apiClient.mutation(TransactionEventReportDocument, {
+        id,
+        amount,
+        type,
+        pspReference: uuidv7(),
+        message: message ?? "Great success!",
+        availableActions: getTransactionActions(type) as TransactionActionEnum[],
+        // TODO: Add externalUrl
+        // externalUrl: ""
+      });
+    }),
 });
