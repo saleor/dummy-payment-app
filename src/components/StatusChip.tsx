615fb30166e9cfbd51d709be71dac77ac4085c97
droniu <droniu@droniu.dev>
2024-07-15 12:30:41 +0200

    better colors

--- src/components/StatusChip.tsx
+++ src/components/StatusChip.tsx
@@ -9,6 +9,8 @@ export const StatusChip = ({ eventType }: StatusChipProps) => {
   switch (eventType) {
     case TransactionEventTypeEnum.ChargeRequest:
     case TransactionEventTypeEnum.AuthorizationRequest:
+    case TransactionEventTypeEnum.CancelRequest:
+    case TransactionEventTypeEnum.RefundRequest:
       return (
         <Chip color="info1" backgroundColor="info1">
           {eventType?.replace(/_/g, " ") ?? "UNKNOWN"}
@@ -23,6 +25,8 @@ export const StatusChip = ({ eventType }: StatusChipProps) => {
       );
     case TransactionEventTypeEnum.ChargeFailure:
     case TransactionEventTypeEnum.AuthorizationFailure:
+    case TransactionEventTypeEnum.CancelFailure:
+    case TransactionEventTypeEnum.RefundFailure:
       return (
         <Chip color="critical1" backgroundColor="critical1">
           {eventType?.replace(/_/g, " ") ?? "UNKNOWN"}
@@ -30,12 +34,14 @@ export const StatusChip = ({ eventType }: StatusChipProps) => {
       );
     case TransactionEventTypeEnum.ChargeSuccess:
     case TransactionEventTypeEnum.AuthorizationSuccess:
+    case TransactionEventTypeEnum.CancelSuccess:
+    case TransactionEventTypeEnum.RefundSuccess:
       return (
         <Chip color="success1" backgroundColor="success1">
           {eventType?.replace(/_/g, " ") ?? "UNKNOWN"}
         </Chip>
       );
-
+    // Chargeback, refund reverse, info, authorization adjustment
     default:
       return (
         <Chip color="default1" backgroundColor="default1" whiteSpace="nowrap">
