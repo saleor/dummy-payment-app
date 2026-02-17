c9537e3fb5409e305b2c687b4de601fab7ca0911
Lukasz Ostrowski <lukasz.ostrowski@saleor.io>
2026-02-17 15:57:26 +0100

    add static payment details and set on webhook

--- src/modules/validation/sync-transaction.ts
+++ src/modules/validation/sync-transaction.ts
@@ -9,16 +9,3 @@ export const dataSchema = z.object({
 });
 
 export type SyncWebhookRequestData = z.infer<typeof dataSchema>;
-
-export const responseSchema = z.object({
-  pspReference: z.string().optional(),
-  result: transactionEventTypeSchema,
-  amount: z.number(),
-  data: z.record(z.union([z.string(), z.number(), z.boolean()])).optional(),
-  time: z.string().optional(),
-  externalUrl: z.string().url().optional(),
-  message: z.string().optional(),
-  actions: transactionActionsSchema,
-});
-
-export type ResponseType = z.infer<typeof responseSchema>;
